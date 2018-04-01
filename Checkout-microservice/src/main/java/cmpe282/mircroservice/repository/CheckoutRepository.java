9f3db8ca4e9ec368c9d7da5d4f8f1058d5dda531
Tsung Min Huang <tsungmin.huang@sjsu.edu>
2018-03-31 20:15:49 -0700

    Remove useless code

--- Checkout-microservice/src/main/java/cmpe282/mircroservice/repository/CheckoutRepository.java
+++
@@ -1,9 +0,0 @@
-package cmpe282.mircroservice.repository;
-
-import cmpe282.mircroservice.model.Checkout;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CheckoutRepository extends MongoRepository<Checkout,String> {
-
-}
-
